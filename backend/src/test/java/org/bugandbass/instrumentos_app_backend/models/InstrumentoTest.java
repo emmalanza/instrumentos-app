626f4ae85855a775c2d840937c8f6b391d6d0609
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-20 17:30:41 +0100

    test gettersInstrumento failed

--- backend/src/test/java/org/bugandbass/instrumentos_app_backend/models/InstrumentoTest.java
+++ backend/src/test/java/org/bugandbass/instrumentos_app_backend/models/InstrumentoTest.java
@@ -1,5 +1,6 @@
 package org.bugandbass.instrumentos_app_backend.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -8,11 +9,22 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class InstrumentoTest {
+    Instrumento instrumento;
+    @BeforeEach
+    void setUp() {
+        instrumento = new Instrumento("Piano", 1, "Cuerda") {};
+    }
     @Test
     void testInstanciaInstrumento() {
-        Instrumento instrumento = new Instrumento("Piano"){};
         assertThat(instrumento, is(notNullValue()));
         assertThat(instrumento, is(instanceOf(Instrumento.class)));
+    }
+    @Test
+    void testGettersIntrumento() {
+
+        assertThat(instrumento.getNombre(), is("Piano"));
+        assertThat(instrumento.getId(), is(1));
+        assertThat(instrumento.getTipo(), is("Cuerda"));
     }
 
 }
