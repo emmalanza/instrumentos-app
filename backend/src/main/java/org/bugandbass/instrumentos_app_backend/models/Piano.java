09552c1d8a4027aa77f35bf621ec5ce437ecc419
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-22 13:28:15 +0100

    STATUS OK tests de Piano refactorizado para cambiar el método tocarInstrumento de List<Integer> a List<Nota>

--- backend/src/main/java/org/bugandbass/instrumentos_app_backend/models/Piano.java
+++ backend/src/main/java/org/bugandbass/instrumentos_app_backend/models/Piano.java
@@ -8,7 +8,21 @@ public class Piano extends Instrumento{
 
     }
     @Override
-    public List<Integer> tocarInstrumento() {
-        return List.of(1, 2, 3);
+    public List<Nota> tocarInstrumento() {
+        return List.of(
+            new Nota("A", "C4"),
+            new Nota("W", "C#4"),
+            new Nota("S", "D4"),
+            new Nota("E", "D#4"),
+            new Nota("D", "E4"),
+            new Nota("F", "F4"),
+            new Nota("T", "F#4"),
+            new Nota("J", "G4"),
+            new Nota("Y", "G#4"),
+            new Nota("K", "A4"),
+            new Nota("U", "A#4"),
+            new Nota("L", "B4"),
+            new Nota("Ñ", "C5")
+        );
     }
 }
