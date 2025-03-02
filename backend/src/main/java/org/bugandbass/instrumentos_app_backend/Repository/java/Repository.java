70893c630921026f774fc5dc56616469a8b82fad
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-03-02 12:12:53 +0100

    Resuelve conflictos de merge en Repository.java durante la fusión de 'dev'

--- backend/src/main/java/org/bugandbass/instrumentos_app_backend/Repository/java/Repository.java
+++ backend/src/main/java/org/bugandbass/instrumentos_app_backend/Repository/java/Repository.java
@@ -2,6 +2,7 @@ package org.bugandbass.instrumentos_app_backend.Repository.java;
 
 import org.bugandbass.instrumentos_app_backend.models.Grabacion;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,7 +14,7 @@ public class Repository {
     private List<Grabacion> grabaciones;
     private int id = 1;
 
-    private Repository() {
+    public Repository() {
         this.grabaciones = this.iniciarArray();
     }
 
@@ -43,7 +44,7 @@ public class Repository {
                 Map.of("C3", "1"),
                 Map.of("C3", "1")
         );
-        
+
         grabaciones.add(new Grabacion(id++, "titulo1", cancion1));
         grabaciones.add(new Grabacion(id++, "titulo2", cancion2));
         grabaciones.add(new Grabacion(id++, "titulo3", cancion3));
@@ -52,18 +53,10 @@ public class Repository {
 
     public void addGrabacion(String json) {
         Gson gson = new Gson();
-
-        List<Map<String, String>> listaNotas = gson.fromJson(json, new TypeToken<List<Map<String, String>>>() {}.getType());
-
+        List<Map<String, String>> listaNotas = gson.fromJson(json, new TypeToken<List<Map<String, String>>>() {
+        }.getType());
         Grabacion nuevaGrabacion = new Grabacion(id, "titulo" + id++, listaNotas);
-        
         grabaciones.add(nuevaGrabacion);
-
-        Grabacion grabacion = gson.fromJson(json, Grabacion.class);
-        if (grabacion != null) {
-            grabacion.setId(id++);
-            grabaciones.add(grabacion);
-        }
     }
 
     public void deleteGrabacion(String json) {
